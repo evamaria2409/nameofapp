d3cac5227272afb3607ff46268bb5e1a3b71ea80
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-03-06 22:07:16 +0100

    4.2

--- db/migrate/20170204190114_rename_colors_in_products.rb
+++ db/migrate/20170204190114_rename_colors_in_products.rb
@@ -1,5 +1,5 @@
-class RenameColorsInProducts < ActiveRecord::Migration[5.0]
+class RenameColorsInProducts < ActiveRecord::Migration
   def change
-  	 rename_column :products, :color, :colour
+    rename_column :products, :color, :colour
   end
 end
