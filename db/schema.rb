73277ed7b0547f832d2cf5dc83cb5b94301abdc2
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-03-06 21:29:15 +0100

    4.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203212633) do
+ActiveRecord::Schema.define(version: 20170204190114) do
 
   create_table "object_names", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170203212633) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
   end
 
 end
