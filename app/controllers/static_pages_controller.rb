5e34c40af29d460259386b39e2a701273116c7ee
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-03-12 09:52:01 +0100

    4.5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,14 @@
 class StaticPagesController < ApplicationController
+  layout "static"
   def index
   end
 
   def landing_page
     @featured_product = Product.first
+  end
+
+  def landing_page
+  @products = Product.limit(3)
   end
 
   def landing_page.html.erb
