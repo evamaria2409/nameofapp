3caf398c0e8d7f9061875a93546a50b864e55bcc
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-04-24 20:07:41 +0200

    5.2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,14 @@ class ProductsController < ApplicationController
 
   # GET /products
   # GET /products.json
-  def index
+def index
+  if params[:q]
+    search_term = params[:q]
+    @products = Product.search(search_term)
+  else
     @products = Product.all
   end
+end
 
   # GET /products/1
   # GET /products/1.json
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
 	has_many :orders
 end
+
+def self.search(search_term)
+  Product.where("name LIKE ?", "%#{search_term}%")
+end
