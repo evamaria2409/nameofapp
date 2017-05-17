507e6ff57c8c4b1511feac20bc158df873336e26
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-05-17 21:36:18 +0200

    comment function

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
     has_many :orders
+    has_many :comments
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
+  end
+  def highest_rating_comment
+  comments.rating_desc.first
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502182228) do
+ActiveRecord::Schema.define(version: 20170517190628) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.integer  "rating"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "object_names", force: :cascade do |t|
     t.datetime "created_at", null: false
