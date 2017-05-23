8e60c608d075388d6b36dc1640f849f805a36a76
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-05-23 21:15:03 +0200

    changed to field in the mailer to my email adress

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
-        :to => 'your-email@example.com',
+        :to => 'rietscheleva@yahoo.de',
         :subject => "A new contact form message from #{name}")
   end
 
