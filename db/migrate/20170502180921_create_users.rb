10f111dd87b5c5d72185b6d4b766abc03ed68d0c
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-06-05 12:25:10 +0200

    5.10 problem with migration

--- db/migrate/20170502180921_create_users.rb
+++ db/migrate/20170502180921_create_users.rb
@@ -1,5 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
-     add_column :users, :admin, :boolean, default: false, null: false
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+     
+    end
   end
 end
