4edbb015682b21b6b74303e337aeaa6ec736e4d2
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-06-05 13:23:23 +0200

    5.11

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,16 @@ class UserMailer < ApplicationMailer
   default from: "from@example.com"
 
   def contact_form(email, name, message)
-  @message = message
+  	@message = message
     mail(:from => email,
         :to => 'rietscheleva@yahoo.de',
         :subject => "A new contact form message from #{name}")
+  end
+
+  def welcome(user)
+  	@appname = "Pulp Cosmetics"
+  	mail(to: user.email,
+       subject: "Welcome to #{@appname}!")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, controllers: { registrations: "user_registrations" }
   resources :users
   resources :products do
   resources :comments
