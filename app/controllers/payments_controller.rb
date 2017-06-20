53de6e90a2446b99c37c3e46e7fb5122656ec1f8
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-06-20 13:16:21 +0200

    6.6 payments_controller changes

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,39 +1,34 @@
 class PaymentsController < ApplicationController
 	
-	def create
+    def create
+        token = params[:stripeToken]
+        @product = Product.find(params[:product_id])
+        @user = current_user
 
-    	@product = Product.find(params[:product_id])
-    	if user_signed_in?
-      		@user = current_user
-    	else
-      		@user = User.new(id: 99)
-    	end
-    
-    	token = params[:stripeToken]
-    	begin
-      		charge = Stripe::Charge.create(
-		        amount: (@product.price * 100).to_i,
-		        currency: "eur",
-		        source: token,
-		        description: @product.description,
-		        receipt_email: params[:stripeEmail]
-		    )
+        begin
+            charge = Stripe::Charge.create(
+                amount: (@product.price*100).to_i,
+                currency: "gbp",
+                source: token,
+                description: params[:stripeEmail]
+            )
 
-		    if charge.paid
-		       Order.create(
-		        user_id: @user.id,
-		        product_id: params[:product_id],
-		        total: @product.price
-		       )
-		       flash[:notice] = "Thank you for purchasing #{@product.name}"
-		    end
+            if charge.paid
+                Order.create(
+                    product_id: @product.id,
+                    user_id: @user.id,
+                    total: @product.price
+                )
+            end
 
-    rescue Stripe::CardError => e
-      body = e.json_body
-      err = body[:error]
-      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    flash[:success] = "Your payment was processed successfully"
+
+        rescue Stripe::CardError => e
+            body = e.json_body
+            err = body[:error]
+            flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+        end
+
+        redirect_to product_path(@product)
     end
-    redirect_to product_path(@product)
-  end
-
 end
