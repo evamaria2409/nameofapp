5dd954b2bde75534727fa6192737995a0f30a8c9
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-07-04 14:01:16 +0200

    error in products_controller and fix of will_paginate in products/show.html.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,9 +20,9 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC")
+    @comments = @product.comments.paginate(page: params[:page], per_page: 5)
 
     logger.debug "Comments: #{@comments}"
-
   end
 
   # GET /products/new
