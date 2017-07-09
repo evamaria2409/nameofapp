79a66e52d42427c557d4246700811730856a4bd2
Eva Maria Rietschel <rietscheleva@yahoo.de>
2017-07-09 20:50:41 +0200

    redis

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Nameofapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+  config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
   end
 end
 
-config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
